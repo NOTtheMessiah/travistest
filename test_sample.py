56f43edfe52a64ccd923fc2b2141918a8fb42b5c
Brian Cohen <brian.cohen.88@gmail.com>
2018-11-27 12:03:07 -0500

    added successful test

--- test_sample.py
+++ test_sample.py
@@ -1,4 +1,3 @@
-# content of test_sample.py
 def inc(x):
     return x + 1
 
@@ -6,3 +5,6 @@ def inc(x):
 def test_answer():
     assert inc(3) == 5
 
+
+def test_correctanswer():
+    assert inc(3) == 4
